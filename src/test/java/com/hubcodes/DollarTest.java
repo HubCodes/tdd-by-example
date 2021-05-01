c036436c8b1114815fac3c59274122f3390eed28
Taemin Cha <krlrhkstk12@gmail.com>
2021-05-01 10:56:51 +0900

    Add Dollar.equals to compare dollars

--- src/test/java/com/hubcodes/DollarTest.java
+++ src/test/java/com/hubcodes/DollarTest.java
@@ -3,6 +3,7 @@ package com.hubcodes;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class DollarTest {
 
@@ -13,5 +14,11 @@ public class DollarTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(6));
     }
 }
