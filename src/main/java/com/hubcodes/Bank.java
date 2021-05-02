8b65e2bc14106851c4a40044b3050bb1ba366198
Taemin Cha <krlrhkstk12@gmail.com>
2021-05-02 11:42:59 +0900

    Add converting currencies

--- src/main/java/com/hubcodes/Bank.java
+++ src/main/java/com/hubcodes/Bank.java
@@ -1,7 +1,41 @@
 package com.hubcodes;
 
+import java.util.HashMap;
+
 public class Bank {
+    private static class Pair {
+        private String from;
+        private String to;
+
+        Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public boolean equals(Object o) {
+            Pair pair = (Pair) o;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        public int hashCode() {
+            return 0;
+        }
+    }
+
+    private HashMap<Pair, Integer> rates = new HashMap<>();
+
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    int rate(String from, String to) {
+        if (from.equals(to)) {
+            return 1;
+        }
+        return rates.get(new Pair(from, to));
+    }
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
     }
 }
